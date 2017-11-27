996d74a0ec67210ed74f8c20d9f3df86d9b14131
Anthony Ward <q5233000@live.tees.ac.uk>
2017-11-27 11:31:55 +0000

    Revert "Merge branch 'Messaging-dev'"
    This reverts commit e162e377a4aca7a72e915887e32d4f5bbb50017b, reversing
    changes made to aa7edc935a731c908d581bd796da9a5c23b90acc.

--- MessagingService/MessagingService/Startup.cs
+++ MessagingService/MessagingService/Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using MessagingService.Models;
 
 namespace MessagingService
 {
@@ -25,7 +23,6 @@ namespace MessagingService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MessageContext>(opt => opt.UseInMemoryDatabase("MessageList"));
             services.AddMvc();
         }
 
